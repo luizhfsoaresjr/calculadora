9e479e70f09eccccbf867750e1ee795ad056c341
Luiz Henrique <luizhfsoaresjr@gmail.com>
2022-05-15 22:48:05 -0300

    First commit

---
+++ R/soma.R
@@ -0,0 +1,14 @@
+#' Função para somar
+#'
+#' Esta função serve para somar dois números
+#'
+#' @param a
+#' @param b
+#'
+#' @examples
+#' somar(3,4)
+#'
+#' @export
+somar <- function(a,b){
+  a + b
+}
---
+++ R/subtrair.R
@@ -0,0 +1,14 @@
+#' Função para subtrair
+#'
+#' Esta função serve para subtrair dois números
+#'
+#' @param a
+#' @param b
+#'
+#' @examples
+#' somar(3,4)
+#'
+#' @export
+subtrair <- function(a,b){
+  a-b
+}
